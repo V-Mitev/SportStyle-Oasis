97f927236faba92d4f375809af5e2589bdf66300
Vladimir Mitev <vmitev2001@abv.bg>
2024-01-02 18:00:44 +0000

    Initialize project

---
+++ SportStyle-Oasis-Web/SportStyle-Oasis/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SportStyle_Oasis.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
