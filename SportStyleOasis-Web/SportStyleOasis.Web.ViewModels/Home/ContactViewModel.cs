32e901be4713fd905d83c3ac3549a1542084e885
Vladimir Mitev <vmitev2001@abv.bg>
2024-03-19 19:38:42 +0200

    Add contact form to can users send email via send grid

---
+++ SportStyleOasis-Web/SportStyleOasis.Web.ViewModels/Home/ContactViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace SportStyleOasis.Web.ViewModels.Home
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class ContactViewModel
+    {
+        [Required]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        public string Message { get; set; } = null!;
+    }
+}
--- SportStyleOasis-Web/SportStyleOasis/Controllers/HomeController.cs
+++ SportStyleOasis-Web/SportStyleOasis/Controllers/HomeController.cs
@@ -1,13 +1,19 @@
 ﻿namespace SportStyleOasis.Controllers
 {
     using Microsoft.AspNetCore.Mvc;
+    using SendGrid;
+    using SendGrid.Helpers.Mail;
     using SportStyleOasis.Web.Infrastructure.Extensions;
+    using SportStyleOasis.Web.ViewModels.Home;
     using static Common.GeneralApplicationConstants;
 
     public class HomeController : Controller
     {
-        public HomeController()
+        private readonly IConfiguration configuration;
+
+        public HomeController(IConfiguration configuration)
         {
+            this.configuration = configuration;
         }
 
         public IActionResult Index()
@@ -34,6 +40,42 @@
             }
 
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult Contact()
+        {
+            var model = new ContactViewModel();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Contact(ContactViewModel model)
+        {
+            try
+            {
+                await SendMail();
+            }
+            catch (Exception e)
+            {
+                throw new InvalidOperationException(e.Message);
+            }
+
+            return View();
+        }
+
+        public async Task SendMail()
+        {
+            var apiKey = configuration["SendGridApiKey:apiKey"];
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("vmitev2001@abv.bg", "Vladimir");
+            var subject = "Sending with SendGrid is Fun";
+            var to = new EmailAddress("vladimirmitev6969@gmail.com", "Vladimir Mitev");
+            var plainTextContent = "and easy to do anywhere, even with C#";
+            var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
         }
     }
 }
