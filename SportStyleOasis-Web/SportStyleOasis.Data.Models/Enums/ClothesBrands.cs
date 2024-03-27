bcf9f0c4b5610722d40d0efeedb0ccf32e3e7f75
Vladimir Mitev <vmitev2001@abv.bg>
2024-03-27 16:14:15 +0200

    Add attribute display name for clothe brands

--- SportStyleOasis-Web/SportStyleOasis.Data.Models/Enums/ClothesBrands.cs
+++ SportStyleOasis-Web/SportStyleOasis.Data.Models/Enums/ClothesBrands.cs
@@ -1,10 +1,14 @@
 ﻿namespace SportStyleOasis.Data.Models.Enums
 {
+    using System.ComponentModel.DataAnnotations;
+
     public enum ClothesBrands
     {
         Nike = 0, 
         Puma = 1,
+        [Display(Name ="My protein")]
         MyProtein = 2,
+        [Display(Name ="Under Armour")]
         UnderArmour = 3,
         Adidas = 4,
     }
