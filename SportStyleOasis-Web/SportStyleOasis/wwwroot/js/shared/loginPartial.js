aba8289c1f7bb350a56eda013c25e7e8d8d352a3
Vladimir Mitev <vmitev2001@abv.bg>
2024-04-01 14:41:19 +0300

    Separate from views css and js code to onother files

---
+++ SportStyleOasis-Web/SportStyleOasis/wwwroot/js/shared/loginPartial.js
@@ -0,0 +1,13 @@
+﻿function goToShoppingCart(cartId) {
+    $.ajax({
+        url: '/ShoppingCart/ViewShoppingCart',
+        type: 'GET',
+        data: { cartId },
+        success: function (response) {
+            window.location.href = '/ShoppingCart/ViewShoppingCart?cartId=' + cartId;
+        },
+        error: function (error) {
+            alert("An error occurred while processing your request. Please try again later.");
+        }
+    });
+}
