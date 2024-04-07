d9c223087539f3f3e4a66b923983a98f271596d0
Vladimir Mitev <vmitev2001@abv.bg>
2024-04-07 15:40:09 +0300

    Add thee models for every collection

--- SportStyleOasis-Web/SportStyleOasis/wwwroot/js/home/index.js
+++ SportStyleOasis-Web/SportStyleOasis/wwwroot/js/home/index.js
@@ -3,3 +3,21 @@
 
     window.location.href = url;
 }
+
+function goToMaleCollection() {
+    var url = '/Clothes/All?Gender=Male';
+
+    window.location.href = url;
+}
+
+function goToFemaleCollection() {
+    var url = '/Clothes/All?Gender=Female';
+
+    window.location.href = url;
+}
+
+function goToProteinCollection() {
+    var url = '/ProteinPowder/All';
+
+    window.location.href = url;
+}
