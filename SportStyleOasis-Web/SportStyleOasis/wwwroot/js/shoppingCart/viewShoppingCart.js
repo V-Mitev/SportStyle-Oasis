76f63e1688052c1190739b4bc8cb339b7da14160
Vladimir Mitev <vmitev2001@abv.bg>
2024-04-12 16:08:26 +0300

    Edit checkValue function

--- SportStyleOasis-Web/SportStyleOasis/wwwroot/js/shoppingCart/viewShoppingCart.js
+++ SportStyleOasis-Web/SportStyleOasis/wwwroot/js/shoppingCart/viewShoppingCart.js
@@ -1,5 +1,6 @@
 ﻿const step = 1;
 const minimumQuantityForOrder = 1;
+var debounceTimer;
 
 function removeClothe(element) {
     const url = '/ShoppingCart/RemoveClothFromCart';
@@ -70,29 +71,45 @@ function increaseQuantity(element, availableQuantity, clothInventoryId) {
         alert(`The available quantity of this product is ${availableQuantity}.`);
     } else {
         quantityField.value = currentValue;
+        updateClothOrderedQuantity(clothInventoryId, currentValue);
     }
+}
 
-    updateClothOrderedQuantity(clothInventoryId, currentValue);
+function checkValueDebounced(inputField, maxQuantity, inventoryId, type) {
+    // Clear the previous timer
+    clearTimeout(debounceTimer);
+
+    // Set a new timer to run the validation function after a delay
+    debounceTimer = setTimeout(function () {
+        checkValue(inputField, maxQuantity, inventoryId, type);
+    }, 500); // Adjust the delay as needed (e.g., 500 milliseconds)
 }
 
 function checkValue(inputField, maxQuantity, inventoryId, type) {
-    var currentValue = parseInt(inputField.value);
-    var isNan = isNaN(currentValue);
+    var currentValue = inputField.value;
 
-    if (currentValue < minimumQuantityForOrder || isNan) {
-        alert('Quantity cannot be less than 1 or empty.');
+    // Check for empty string because when user delete with backspace cath check for quantity can't be less than 1.
+    // And pop up alert for this if input is empty i didn't update db quantity and give to user this option.
+    if (currentValue != '') {
+        var currentValueAsInt = parseInt(currentValue);
 
-        inputField.value = minimumQuantityForOrder;
-    } else if (currentValue > maxQuantity) {
-        alert('Quantity cannot exceed available stock.');
+        if (currentValueAsInt < minimumQuantityForOrder) {
+            alert('Quantity cannot be less than 1.');
 
-        inputField.value = maxQuantity;
-    }
+            inputField.value = minimumQuantityForOrder;
+            return;
+        } else if (currentValueAsInt > maxQuantity) {
+            alert(`Availble stock quantity is ${maxQuantity}`);
 
-    if (type == 'cloth') {
-        updateClothOrderedQuantity(inventoryId, currentValue);
-    } else {
-        updateProteinOrderedQuantity(inventoryId, currentValue);
+            inputField.value = maxQuantity;
+            return;
+        }
+
+        if (type == 'cloth') {
+            updateClothOrderedQuantity(inventoryId, currentValueAsInt);
+        } else {
+            updateProteinOrderedQuantity(inventoryId, currentValueAsInt);
+        }
     }
 }
 
