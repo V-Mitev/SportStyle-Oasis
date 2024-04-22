b05d3a61f3a410bc2cd3853d3aedf35e0ba89f07
Vladimir Mitev <vmitev2001@abv.bg>
2024-04-22 11:59:27 +0300

    Edit view all users in admin area and separete css and js code

---
+++ SportStyleOasis-Web/SportStyleOasis/wwwroot/js/admin/user/all.js
@@ -0,0 +1,19 @@
+﻿function confirmDelete(userId, userFullName) {
+    const url = '/Admin/User/Delete';
+
+    if (confirm(`Are you sure you want to delete ${userFullName} ?`)) {
+        $.ajax({
+            url: url,
+            type: 'POST',
+            data: { userId, userFullName },
+            success: function (result) {
+                location.reload();
+            },
+            error: function (error) {
+                alert('An error occurred while removing user. Please try again later.');
+            }
+        });
+    } else {
+        location.reload();
+    }
+}
